8bcb432a9e501f8172fdf23d57991769fdf6053c
jcoquinn <9020588-jcoquinn@users.noreply.gitlab.com>
2025-07-11 21:40:01 -0400

    reworking the config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,26 +1,32 @@
+export interface Config {
+	url: string;
+	serviceUrl: string;
+	returnUrl: string;
+}
+
 export default {
 	default: ({ env }) => ({
-		// CAS application URL. E.g. https://example.com/cas
+		// CAS application URL.
+		// E.g. https://example.com/cas
 		url: env('CAS_URL'),
 		// Strapi backend URL to use as the service param in CAS login and validation requests.
-		// E.g. http[s]://<host>[:port]/api/cas/callback
-		serviceUrl: env('CAS_SERVICE_URL'),
-		// Frontend URL to redirect to after login. Used to pass access token.
+		// E.g. http://localhost:1337/api/cas/callback
+		serviceUrl: `${env('CAS_STRAPI_URL')}/api/cas/callback`,
+		// Client URL to redirect to after authentication. Used to pass access token.
+		// E.g. http://localhost:3000/login
 		returnUrl: env('CAS_RETURN_URL'),
 	}),
-	validator: (config: { url: string; serviceUrl: string }) => {
-		const env = {
-			url: 'CAS_URL',
-			serviceUrl: 'CAS_SERVICE_URL',
-			returnUrl: 'CAS_RETURN_URL',
-		};
-		for (const [k, v] of Object.keys(config).entries()) {
+	validator: (config: Config) => {
+		for (const [k, v] of Object.entries(config)) {
 			if (!v) {
-				throw new Error(`${env[k]} is required`);
+				throw new Error(`${k} is required`);
 			}
-			config[k] = v.trim().replace(/\/+$/, '');
-			if (config[k] === '') {
-				throw new Error(`${env[k]} invalid value: ${config[k]}`);
+			config[k] = v.trim();
+			if (/url$/i.test(k)) {
+				config[k].replace(/\/+$/, ''); // remove trailing slash
+				if (!/^https?:\/\//.test(v)) {
+					throw new Error(`${k} invalid URL: ${v}`);
+				}
 			}
 		}
 	},
